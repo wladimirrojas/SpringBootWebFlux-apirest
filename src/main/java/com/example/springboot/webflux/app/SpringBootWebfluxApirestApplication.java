d39a9347dee04d295d79c9bc075d48e8ab508c73
wladimirrojas <rajimiru@gmail.com>
2023-08-02 01:28:20 -0400

    first commit

---
+++ src/main/java/com/example/springboot/webflux/app/SpringBootWebfluxApirestApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot.webflux.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebfluxApirestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebfluxApirestApplication.class, args);
+    }
+
+}
