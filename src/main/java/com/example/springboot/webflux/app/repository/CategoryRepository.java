f851a2d8ace64eaf980d98d4b715d45afc9d7aa5
wladimirrojas <rajimiru@gmail.com>
2023-08-21 01:16:09 -0400

    added method to find product and category by name for testing

--- src/main/java/com/example/springboot/webflux/app/repository/CategoryRepository.java
+++ src/main/java/com/example/springboot/webflux/app/repository/CategoryRepository.java
@@ -3,6 +3,9 @@ package com.example.springboot.webflux.app.repository;
 
 import com.example.springboot.webflux.app.models.Category;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Mono;
 
 public interface CategoryRepository extends ReactiveMongoRepository<Category, String> {
+
+    Mono<Category> findByName(String name);
 }
--- src/main/java/com/example/springboot/webflux/app/repository/ProductRepository.java
+++ src/main/java/com/example/springboot/webflux/app/repository/ProductRepository.java
@@ -3,6 +3,9 @@ package com.example.springboot.webflux.app.repository;
 
 import com.example.springboot.webflux.app.models.Product;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Mono;
 
 public interface ProductRepository extends ReactiveMongoRepository<Product, String> {
+
+    Mono<Product> findByName(String name);
 }
